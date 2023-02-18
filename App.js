8ba63583061ee794f0b3fa309b444d4120a54df5
Varun A C <96919265+acvarun@users.noreply.github.com>
2023-02-18 17:23:36 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
+$(document).ready(function () {
 
+    console.log("Hello World!");
+    var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+
+    if (this.readyState == 4 && this.status == 200) {
+
+     console.log(this.responseText);
+
+    }
+
+  };
+
+  xhttp.open("GET", "D:\Newton school\Myntra-Clone---React-Project---pw6th899sfbm\Product.json", true);
+
+  xhttp.send();
+    
+    });
